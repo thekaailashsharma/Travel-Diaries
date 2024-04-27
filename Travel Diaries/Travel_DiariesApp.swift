6ba3af0dadfdd7d3e149e6f1752d41ae38de199e
thekaailashsharma <kailashps.1011@gmail.com>
2024-04-28 00:18:55 +0530

    Initial Commit

---
+++ Travel Diaries/Travel_DiariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_DiariesApp.swift
+//  Travel Diaries
+//
+//  Created by Kailash on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Travel_DiariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
